e2b9b32215e4929566160e21930b5ffbe407bc9f
javh000 <jaime_v000@hotmail.com>
2024-06-20 18:03:48 -0300

    Connect Alwaysdata database using environment variables

--- db/db.js
+++ db/db.js
@@ -1,15 +1,16 @@
+require("dotenv").config();
 const mysql = require("mysql2");
 
 const connection = mysql.createConnection({
-  host: "localhost",
-  user: "root",
-  password: "",
-  database: "store_catalog",
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASS,
+  database: "javh_store_catalog",
 });
 
 connection.connect((err) => {
   if (err) {
-    console.log("Error connecting to de database");
+    console.log("Error connecting to de database", err);
     return;
   }
   console.log("Connecting to de database");
