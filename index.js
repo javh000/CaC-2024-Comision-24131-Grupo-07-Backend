924c115d621c52b0e183cde002f342e4d4a6b70f
javh000 <jaime_v000@hotmail.com>
2024-06-29 14:12:07 -0300

    process.env.PORT

--- index.js
+++ index.js
@@ -10,6 +10,6 @@ app.get("/", (req, res) => {
   res.send("Bienvenido a la API");
 });
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => console.log(`http://localhost:${PORT}`));
